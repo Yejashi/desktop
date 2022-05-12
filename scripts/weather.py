1988f9203330d6bcdfb86d24861c4e7abfc549ea
yejashi <befikirteferi@gmail.com>
2022-05-12 02:31:07 -0400

    Second commit

---
+++ scripts/weather.py
@@ -0,0 +1,15 @@
+#!python3
+from gettext import find
+import requests, bs4
+
+req = requests.get('https://weather.com/weather/today/l/225c08713830ede82389fa0bc8608176d8251d86a31c94656693a9e344650282')
+
+finder = bs4.BeautifulSoup(req.text, 'lxml')
+
+x = finder.find('span', {"data-testid": "TemperatureValue"})
+x2 = finder.find('span', {"data-testid": "PercentageValue"})
+
+weather = x.text
+humidity = x2.text
+
+print(weather + " " + humidity)
